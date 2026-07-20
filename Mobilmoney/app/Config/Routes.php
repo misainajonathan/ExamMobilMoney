e5a42195565e8a762d18a70e69073e462024449b
mandresy10 <andriamahenintsoamandresy11@gmail.com>
2026-07-20 12:15:40 +0300

    groupe de routes /client avec filtre de session v1

--- Mobilmoney/app/Config/Routes.php
+++ Mobilmoney/app/Config/Routes.php
@@ -7,3 +7,6 @@ $routes->get('/', 'Home::index');
 
 $routes->get('/login', 'Login::index');
 $routes->post('/login', 'Login::authenticate');
+$routes->group('client', ['filter' => 'session'], static function (RouteCollection $routes): void {
+	$routes->get('/', static fn (): string => 'Client area');
+});
