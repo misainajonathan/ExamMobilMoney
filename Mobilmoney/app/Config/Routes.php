4e977edc8e8d0be612eb745b5e40363dfabcebbb
mandresy10 <andriamahenintsoamandresy11@gmail.com>
2026-07-20 11:47:01 +0300

    routes-login-v1

--- Mobilmoney/app/Config/Routes.php
+++ Mobilmoney/app/Config/Routes.php
@@ -4,3 +4,6 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+$routes->get('/login', 'Login::index');
+$routes->post('/login', 'Login::authenticate');
